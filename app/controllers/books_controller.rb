e1b22c59b76622935e13430a16d1798fae46c020
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-02-08 12:22:18 +0000

    Bookersを作成しよう続き

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  resources :books
+  root 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

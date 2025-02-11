1b8b0b3963184e51796934b17926d78fd27528a4
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-02-11 12:28:05 +0000

    Bookersを作成しよう続き

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all 
     @book = Book.new
-
   end
 
   def new
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to booker_path(book.id)
+    redirect_to book_path(book.id)
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  post 'books' => 'books#create'
   resources :books
   root 'homes#top'
-  get 'books/:id' => 'books#show', as: 'booker'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

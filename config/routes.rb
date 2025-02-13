fe9f35da25b7c58ec214db78e73edd52675abe7a
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-02-13 10:30:58 +0000

    Bookersを作成しよう続き

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
   resources :books
-  delete 'book/:id' => 'books#destroy', as:'destroy-abook'
+  delete '/books/:id' => 'books#destroy', as:'destroy-abook'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

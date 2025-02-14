9276d1f59e26e1a8deab267fec28c4710f854669
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-02-14 15:02:52 +0000

    Bookersを作成しよう続き

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def show
@@ -21,6 +22,9 @@ class BooksController < ApplicationController
   end
 
   def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'homes#top'
   resources :books
   delete '/books/:id' => 'books#destroy', as:'destroy-abook'
+  patch '/books/:id' => 'books#update', as:'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
